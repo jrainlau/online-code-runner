65a029640bbf97e1494217af26dabc2d3e660003
jrainliu <jrainliu@tencent.com>
2021-07-15 19:32:10 +0800

    chore: rebuild

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = {
   outputDir: './docs',
+  publicPath: '/online-code-runner',
   configureWebpack: {
     plugins: [
       new MonacoWebpackPlugin(),
